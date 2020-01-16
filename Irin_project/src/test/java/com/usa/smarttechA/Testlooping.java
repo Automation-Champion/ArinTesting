8197ef5fc394131658ca05cf79747bcf09a7403b
mdhaque <mdhaque@mds-macbook-pro.lan>
2020-01-15 19:15:55 -0500

    Commit by Refat & Arin

---
+++ Irin_project/src/test/java/com/usa/smarttechA/Testlooping.java
@@ -0,0 +1,52 @@
+package com.usa.smarttechA;
+
+public class Testlooping {
+	public static void main(String[] args) {
+		int i;
+		for(i = 0;i<=10; i++) {
+		System.out.println(i);	
+		}
+		int j;
+		for(j = 10;j>= 0;j--) {
+			System.out.println("Ans true " +j);
+		}
+		int k;
+		for(k = 1;k<= 10;k+=2) {
+		System.out.println("increse 2:"+k); 
+	}//direction under the parenthesis()
+		int l;
+		for( l = 100;l>=0; l-= 5) {
+		System.out.println("Decrise 5:"+l);
+
+}
+		int a= 0;
+		while(a<=4) {
+			System.out.println("While Loop"+a);
+			a++;//direction under the body
+		}
+		int b = 0;
+		while(b<=10) {
+			System.out.println(b);
+			b++;
+		}
+		int c = 10;
+		while (c>=0) {
+			System.out.println(c);
+			c--;
+		}
+		int d = 0;
+		while(d<=10) {
+			System.out.println(d);
+			d+=2;
+			int e= 10;
+			while(e>=0) {
+				System.out.println(e);
+				e-=2;
+			}
+		}
+		
+	}
+	
+	
+	}
+
